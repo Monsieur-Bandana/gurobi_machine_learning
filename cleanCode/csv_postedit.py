0d2ff0925e67c434bfe72057b76f6060bbba31b5
Your Name Nicolas Wild <nicolas.wild@googlemail.com>
2023-05-08 18:12:49 +0200

    corrected supply problem

--- cleanCode/csv_postedit.py
+++ cleanCode/csv_postedit.py
@@ -3,11 +3,12 @@ import numpy as np
 
 # die vier in csv_create entstandenen Datensätze werden zu einem gestacked
 
-df = pd.read_csv('allRUnsButOnlyTerran.csv')
+df = pd.read_csv('cleanCode/allRUnsButOnlyTerran.csv')
 
 
 # Fraktionskürzel durch echte Bezeichnung ersetzen
 # Es hat sich als effizienter herausgestellt es im Nachhinein über pandas zu bearbeiten, als direkt während des csv create Prozesses
+
 
 conditions = [
     (df['fraction'] == "P"),
@@ -19,7 +20,16 @@ values = ["Protoss", "Terraner", "Zerg"]
 
 df["fraction"] = np.select(conditions, values)
 
+
+# sc2reader zählt supply über das erlaubte limit hinaus
+conditions2 = [
+    (df['supply'] > 200.0),
+]
+
+values2 = [200.0]
+
+df["supply"] = np.select(conditions2, values2)
 # Durch die Konkardination und die Umbenennungen sind ungewünschte Spalten entstanden
 df = df.drop(columns=["Unnamed: 0"])
 
-df.to_csv('allRUnsButOnlyTerran.csv')
+df.to_csv('cleanCode/allRUnsButOnlyTerran.csv')
